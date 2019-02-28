5015b236a6d80d25cace8846592611bd1a1a00ce
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-02-28 16:24:02 -0500

    First Lesson Assignment

--- Clock/Clock/ViewController.swift
+++ Clock/Clock/ViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var timeLabel: UILabel!
-    
+    var programmaticalLabel : UILabel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         timeLabel.textColor = #colorLiteral(red: 1, green: 0.5843137255, blue: 0, alpha: 0.5289223031)
        timeLabel .text = "Current Time"
+        programmaticalLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 250, height: 100))
+        programmaticalLabel?.center = CGPoint(x:self.view.center.x , y: self.view.center.y)
+        programmaticalLabel?.textAlignment = .center
+        programmaticalLabel?.textColor = #colorLiteral(red: 0.9477215409, green: 1, blue: 0, alpha: 1)
+        programmaticalLabel?.text = "I'm a homework !"
+        self.view.addSubview(programmaticalLabel!)
+        
         
     }
 
