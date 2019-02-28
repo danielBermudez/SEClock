997bb517cd3a4f699a1ee5dab5266dbb29481c97
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-02-28 17:16:58 -0500

    Lesson02 Completed

---
+++ Clock/Clock/Clock.swift
@@ -0,0 +1,16 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by Daniel Bermudez on 2/28/19.
+//  Copyright © 2019 Your School. All rights reserved.
+//
+
+import Foundation
+class CLock{
+    
+    var currentTime :  Date{
+        return Date()
+        
+    }
+}
