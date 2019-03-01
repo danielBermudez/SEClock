08ba59eaf4c2a8ab1855fa9f5b621a5dfd52c2a6
Daniel Bermúdez <daniel.bermudez@endava.com>
2019-03-01 10:02:28 -0500

    Lesson03 completed

--- Clock/Clock/ViewController.swift
+++ Clock/Clock/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let clock = CLock()
     
     override func viewDidLoad() {
+        print("viewDidLoad")
         super.viewDidLoad()
         timeLabel.textColor = #colorLiteral(red: 1, green: 0.5843137255, blue: 0, alpha: 0.5289223031)
         let formatter = DateFormatter()
@@ -21,10 +22,20 @@ class ViewController: UIViewController {
        timeLabel .text = "\(formatter.string(from: clock.currentTime ) )"
         
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        
+        timeLabel .text = "\(formatter.string(from: clock.currentTime ) )"
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+      
     }
 
 }
